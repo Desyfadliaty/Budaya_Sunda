e91e0006ed9cc20fea1bbc5aa5b50acf1735ae5f
Riepaz <pandawa.rifaz@gmail.com>
2014-12-14 09:37:43 +0700

    Inherit Alat Musik

--- src/com/Desyfadliaty/budayasunda/BudayaSunda.java
+++ src/com/Desyfadliaty/budayasunda/BudayaSunda.java
@@ -1,16 +1,34 @@
 package com.Desyfadliaty.budayasunda;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class BudayaSunda extends Activity {
 
-
+	private Button btn1;
+	 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.budayasunda);
+		
+		btn1 = (Button) findViewById(R.id.button1);
+		
+		btn1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent("com.Desyfadliaty.budayasunda.AlatMusik"));
+				
+			}
+		});
+		
 		
 	}
 }
